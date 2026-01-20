df9e19b66478b5517c075fefef2c439aba5ee35a
KNSN92 <kekenshin92@gmail.com>
2026-01-20 11:36:11 +0900

    improve: タイトルページにHello World!!を追加

--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -2,6 +2,8 @@ export default function WelcomePage() {
 	return (
 		<div className="text-white size-full flex items-center justify-center text-4xl text-center">
 			ようこそ FunDiaryへ！
+			<br />
+			Hello World!!
 		</div>
 	);
 }
