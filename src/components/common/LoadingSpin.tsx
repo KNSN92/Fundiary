b0d1d2bcf60405c1a3e4be5f90e4c9895396acd9
KNSN92 <kekenshin92@gmail.com>
2026-02-10 09:55:28 +0900

    add: LoadingSpinコンポーネントを追加

--- src/components/common/LoadingSpin.tsx
+++ src/components/common/LoadingSpin.tsx
@@ -1,5 +1,12 @@
-export default function LoadingSpin() {
+import cn from "@/libs/cn";
+
+export default function LoadingSpin({ className }: { className?: string }) {
 	return (
-		<div className="animate-spin h-5 w-5 border-2 border-stone-400 border-t-transparent rounded-full" />
+		<div
+			className={cn(
+				"animate-spin h-5 w-5 border-2 border-stone-400 border-t-transparent rounded-full",
+				className,
+			)}
+		/>
 	);
 }
