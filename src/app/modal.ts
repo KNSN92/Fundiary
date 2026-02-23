f23553d48c8fd6fa742607930732f08f49608adf
KNSN92 <kekenshin92@gmail.com>
2026-02-23 18:56:39 +0900

    docs: 説明を追加

--- src/app/modal.ts
+++ src/app/modal.ts
@@ -3,11 +3,17 @@ import { atom, getDefaultStore, useAtomValue } from "jotai";
 import type { ReactNode } from "react";
 
 export interface Modal {
+	/** モーダル固有id。クローズ処理などで参照されます */
 	id: Identifier;
+	/** 背景色 */
 	bgColor?: string;
+	/** 背景をクリックして閉じられるか */
 	bgClickToClose?: boolean;
+	/** モーダルが開いていても背景が操作可能か。bgClickToCloseの動作が無効になります。 */
 	canControlBg?: boolean;
+	/** モーダルを中央に表示するか */
 	centered?: boolean;
+	/** モーダルの内容を描画する関数 */
 	render: () => ReactNode;
 }
 
@@ -16,6 +22,7 @@ const stackedModalIdentifiers = new Set<Identifier>();
 const store = getDefaultStore();
 
 export class Modals {
+	/** モーダルを表示します。同じidのモーダルを同時に複数表示させることは出来ません。 */
 	show(modal: Modal) {
 		if (stackedModalIdentifiers.has(modal.id)) {
 			console.warn(`Modal with id ${modal.id} is already shown in the stack.`);
@@ -26,6 +33,7 @@ export class Modals {
 		stackedModalIdentifiers.add(modal.id);
 	}
 
+	/** 指定されたidのモーダルを閉じます。 */
 	closeThis(id: Identifier) {
 		const currentStack = store.get(modalStackAtom);
 		const index = currentStack.findIndex((m) => m.id === id);
@@ -37,6 +45,7 @@ export class Modals {
 		}
 	}
 
+	/** 最前面のモーダルを閉じます */
 	close() {
 		const currentStack = store.get(modalStackAtom);
 		const poped = currentStack.pop();
@@ -44,6 +53,7 @@ export class Modals {
 		if (poped) stackedModalIdentifiers.delete(poped.id);
 	}
 
+	/** 全てのモーダルを閉じます */
 	closeAll() {
 		store.set(modalStackAtom, []);
 		stackedModalIdentifiers.clear();
