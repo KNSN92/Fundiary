1263a9826b594642d4f8b6e37193ee574aa565cd
KNSN92 <kekenshin92@gmail.com>
2026-02-23 11:16:09 +0900

    update: claxでやってた縮小ボタンをtailwindのみで行えるように変更し、サイズが1の場合でも無効の赤アイコンになるように変更

--- src/components/diary-pane/edit/EditDiaryPaneGridResizer.tsx
+++ src/components/diary-pane/edit/EditDiaryPaneGridResizer.tsx
@@ -2,77 +2,57 @@ import { MinusCircleIcon, PlusCircleIcon } from "@heroicons/react/24/solid";
 import cn from "@/libs/cn";
 
 interface Props {
-	canResize: { col: boolean; row: boolean };
-	size: { col: number; row: number };
-	setSize: (size: { col: number; row: number }) => void;
+  canResize: { col: boolean; row: boolean };
+  size: { col: number; row: number };
+  setSize: (size: { col: number; row: number }) => void;
 }
 
 export default function EditDiaryPaneGridResizer({
-	canResize,
-	size,
-	setSize,
+  canResize,
+  size,
+  setSize,
 }: Props) {
-	return (
-		<div className="size-24 aspect-square grid grid-cols-[fit-content(50%)_fit-content(50%)_30%] grid-rows-[30%_fit-content(50%)_fit-content(50%)]">
-			<button
-				type="button"
-				disabled={!canResize.col}
-				onClick={() =>
-					setSize({ col: Math.max(1, size.col - 1), row: size.row })
-				}
-				className={cn(
-					"h-full aspect-square col-start-1 flex items-center justify-center rouded-full group",
-					canResize.col && "cursor-pointer",
-				)}
-			>
-				<MinusCircleIcon
-					className={cn(
-						"size-full",
-						canResize.col
-							? "group-hover:brightness-80 fill-white"
-							: "fill-red-500",
-					)}
-				/>
-			</button>
-			<button
-				type="button"
-				onClick={() => setSize({ col: size.col + 1, row: size.row })}
-				className="h-full aspect-square col-start-2 flex items-center justify-center rouded-full cursor-pointer group"
-			>
-				<PlusCircleIcon className="size-full fill-white group-hover:brightness-80" />
-			</button>
-			<button
-				type="button"
-				onClick={() => setSize({ col: size.col, row: size.row + 1 })}
-				className="w-full aspect-square row-start-2 col-start-3 flex items-center justify-center rouded-full cursor-pointer group"
-			>
-				<PlusCircleIcon className="size-full fill-white group-hover:brightness-80" />
-			</button>
-			<button
-				type="button"
-				disabled={!canResize.row}
-				onClick={() =>
-					setSize({ col: size.col, row: Math.max(1, size.row - 1) })
-				}
-				className={cn(
-					"w-full aspect-square row-start-3 col-start-3 flex items-center justify-center rouded-full group",
-					canResize.row && "cursor-pointer",
-				)}
-			>
-				<MinusCircleIcon
-					className={cn(
-						"size-full",
-						canResize.row
-							? "group-hover:brightness-80 fill-white"
-							: "fill-red-500",
-					)}
-				/>
-			</button>
-			<div className="min-w-16 size-full p-2 aspect-square row-start-2 row-span-2 col-span-2 flex items-center justify-center bg-black text-xl text-white font-mono">
-				<span>{size.col}</span>
-				<span>x</span>
-				<span>{size.row}</span>
-			</div>
-		</div>
-	);
+  return (
+    <div className="size-24 aspect-square grid grid-cols-[fit-content(50%)_fit-content(50%)_30%] grid-rows-[30%_fit-content(50%)_fit-content(50%)]">
+      <button
+        type="button"
+        disabled={!canResize.col || size.col <= 1}
+        onClick={() =>
+          setSize({ col: Math.max(1, size.col - 1), row: size.row })
+        }
+        className="group h-full aspect-square col-start-1 flex items-center justify-center rouded-full enabled:cursor-pointer"
+      >
+        <MinusCircleIcon className="size-full group-enabled:group-hover:brightness-80 group-enabled:fill-white group-disabled:fill-red-500" />
+      </button>
+      <button
+        type="button"
+        onClick={() => setSize({ col: size.col + 1, row: size.row })}
+        className="h-full aspect-square col-start-2 flex items-center justify-center rouded-full cursor-pointer group"
+      >
+        <PlusCircleIcon className="size-full fill-white group-hover:brightness-80" />
+      </button>
+      <button
+        type="button"
+        onClick={() => setSize({ col: size.col, row: size.row + 1 })}
+        className="w-full aspect-square row-start-2 col-start-3 flex items-center justify-center rouded-full cursor-pointer group"
+      >
+        <PlusCircleIcon className="size-full fill-white group-hover:brightness-80" />
+      </button>
+      <button
+        type="button"
+        disabled={!canResize.row || size.row <= 1}
+        onClick={() =>
+          setSize({ col: size.col, row: Math.max(1, size.row - 1) })
+        }
+        className="group w-full aspect-square row-start-3 col-start-3 flex items-center justify-center rouded-full enabled:cursor-pointer"
+      >
+        <MinusCircleIcon className="size-full group-enabled:group-hover:brightness-80 group-enabled:fill-white group-disabled:fill-red-500" />
+      </button>
+      <div className="min-w-16 size-full p-2 aspect-square row-start-2 row-span-2 col-span-2 flex items-center justify-center bg-black text-xl text-white font-mono">
+        <span>{size.col}</span>
+        <span>x</span>
+        <span>{size.row}</span>
+      </div>
+    </div>
+  );
 }
