244815f7cfc6342102c4d834f698a8ad8c71e5de
KNSN92 <kekenshin92@gmail.com>
2026-02-23 19:16:06 +0900

    fix: diary paneが見つからない際のエラーで、DiaryPaneのインスタンスIdではなく、検索しようとしたidを表示するように変更

--- src/components/diary-pane/DiaryPane.tsx
+++ src/components/diary-pane/DiaryPane.tsx
@@ -2,27 +2,27 @@ import type { DiaryPaneData } from "fundiary-api/api/diary-pane";
 import fundiary from "@/fundiary";
 
 interface Props {
-	data: DiaryPaneData;
+  data: DiaryPaneData;
 }
 
 export default function Pane({ data }: Props) {
-	const pane = fundiary.diaryPanes.get(data.pane);
-	if (pane == null) {
-		throw new Error(`DiaryPane not found: ${data.id}`);
-	}
-	const { x, y } = data.pos;
-	const { width, height } = data.size;
-	return (
-		<div
-			className="overflow-hidden"
-			style={{
-				gridColumnStart: x + 1,
-				gridColumn: `${x + 1} / ${x + width + 1}`,
-				gridRowStart: y + 1,
-				gridRow: `${y + 1} / ${y + height + 1}`,
-			}}
-		>
-			<pane.component data={data.data} />
-		</div>
-	);
+  const pane = fundiary.diaryPanes.get(data.pane);
+  if (pane == null) {
+    throw new Error(`DiaryPane not found: ${data.pane}`);
+  }
+  const { x, y } = data.pos;
+  const { width, height } = data.size;
+  return (
+    <div
+      className="overflow-hidden"
+      style={{
+        gridColumnStart: x + 1,
+        gridColumn: `${x + 1} / ${x + width + 1}`,
+        gridRowStart: y + 1,
+        gridRow: `${y + 1} / ${y + height + 1}`,
+      }}
+    >
+      <pane.component data={data.data} />
+    </div>
+  );
 }
