19633775f79eb49e680b575bb19806b32734ba5f
KNSN92 <kekenshin92@gmail.com>
2026-02-24 01:10:50 +0900

    feat: トップページにまだ作ってないよ文言を追加

--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -1,9 +1,12 @@
 export default function WelcomePage() {
   return (
-    <div className="text-base-text size-full flex items-center justify-center text-4xl text-center">
-      ようこそ FunDiaryへ！
-      <br />
-      Hello World!!
+    <div className="text-base-text size-full flex flex-col items-center justify-center text-4xl text-center">
+      <div>
+        ようこそ FunDiaryへ！
+        <br />
+        Hello World!!
+      </div>
+      <p className="mt-8 text-base-text-hover text-xl">ここはアプリのトップページになる予定だけどまだ作ってないよ〜</p>
     </div>
   );
 }
